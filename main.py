ecbdd021668cc02d663f6555bfcf0b79369f576e
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2024-11-16 14:58:12 -0800

    Mad Lib assingment

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 adjective0 = input("Give me an adjective: ")
 noun0 = input("Give me a noun: ")
-
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+pluralnoun0 = input("Give me a plural noun: ")
+personinroom0= input ("Give me a person(female):")
+adjective1 = input("Give me an adjective: ")
+clothing0 = input ("Give me a piece of clothing:")
+noun1 = input("Give me a noun: ")
+place0 = input("Give me a place: ")
+pluralnoun1 = input("Give me a plural noun: ")
+adjective2 = input("Give me an adjective: ")
+story = f"There are many {adjective0} ways to \
+choose a/n {noun0} to read. First, you could \
+ask for recommendations from your friends and \
+{pluralnoun0} just don't ask aunt {personinroom0} \
+she only reads {adjective1} books with {clothing0}\
+-ripping goddesses on the cover. If your friends and family are no help, try checking out the {noun1} \
+Review in The {place0} Times. If the {pluralnoun1} featured there are too {adjective2}\
+for your taste, try something a little"
 print(story)
